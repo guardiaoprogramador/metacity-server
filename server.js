388fc7a673e822d94fa0283d5d5437304082968c
guardiaoprogramador <guardiaoprogramador@gmail.com>
2025-11-23 20:10:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,71 +1,67 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+const cors = require('cors');
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
     cors: {
-        origin: "*", // Libera acesso para seu CPanel
+        origin: "*", // Permite conexões de qualquer site
         methods: ["GET", "POST"]
     }
 });
 
-// Armazena os jogadores: { id_socket: { x, y, z, nick, rotation } }
-let players = {};
+let players = {}; // Guarda a posição de todo mundo
 
 io.on('connection', (socket) => {
     console.log('Novo jogador conectado:', socket.id);
 
-    // 1. Jogador entrou no jogo
+    // 1. Jogador entrou
     socket.on('joinGame', (data) => {
-        // Cria o jogador na memória do servidor
         players[socket.id] = {
             id: socket.id,
-            nick: data.nick,
-            x: 0, y: 0, z: 0,
-            rotation: 0,
-            action: 'Idle' // Animação atual
+            nick: data.nick || "Desconhecido",
+            x: 10, y: 0.5, z: 10, // Spawn padrão
+            yaw: 0
         };
-
-        // Avisa o novo jogador sobre quem já está online
-        socket.emit('currentPlayers', players);
-
-        // Avisa os outros que alguém novo entrou
-        socket.broadcast.emit('newPlayer', players[socket.id]);
-        
-        // Mensagem no Chat Global
-        io.emit('chatMessage', { nick: 'Sistema', text: `${data.nick} entrou na cidade.` });
+        // Envia a lista atualizada para TODO MUNDO
+        io.emit('updatePlayers', players);
     });
 
-    // 2. Jogador se moveu
-    socket.on('playerMovement', (movementData) => {
+    // 2. Jogador andou
+    socket.on('playerMove', (data) => {
         if (players[socket.id]) {
-            players[socket.id].x = movementData.x;
-            players[socket.id].y = movementData.y;
-            players[socket.id].z = movementData.z;
-            players[socket.id].rotation = movementData.rotation;
-            players[socket.id].action = movementData.action;
-
-            // Envia a nova posição para TODOS os outros (menos pra quem moveu)
-            socket.broadcast.emit('playerMoved', players[socket.id]);
+            players[socket.id].x = data.x;
+            players[socket.id].y = data.y;
+            players[socket.id].z = data.z;
+            players[socket.id].yaw = data.yaw;
         }
+        // Envia atualização para todos (menos para quem enviou, pra economizar rede)
+        socket.broadcast.emit('updatePlayers', players);
     });
 
     // 3. Chat
-    socket.on('sendChat', (msg) => {
-        io.emit('chatMessage', { nick: msg.nick, text: msg.text });
+    socket.on('chatMsg', (data) => {
+        console.log("Chat recebido:", data.msg);
+        // Reenvia para TODO MUNDO (inclusive quem mandou)
+        io.emit('chatMsg', { 
+            id: socket.id,
+            nick: data.nick, 
+            msg: data.msg 
+        });
     });
 
-    // 4. Desconexão
+    // 4. Desconectou
     socket.on('disconnect', () => {
-        console.log('Saiu:', socket.id);
-        if (players[socket.id]) {
-            io.emit('chatMessage', { nick: 'Sistema', text: `${players[socket.id].nick} saiu.` });
-            delete players[socket.id];
-            io.emit('playerDisconnected', socket.id);
-        }
+        console.log('Jogador saiu:', socket.id);
+        delete players[socket.id];
+        io.emit('updatePlayers', players);
     });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+server.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
 });
